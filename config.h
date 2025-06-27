d47e9631df616d3ba7c084fb21a9148d981c67f9
s7 <vleandromoises@gmail.com>
2025-06-26 23:56:52 -0300

    fix: colors redefined; cyan changed to red

--- config.h
+++ config.h
@@ -10,11 +10,13 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#dd0000";
+static const char col_red[]        = "#dd0000";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
+
+/* Colorbar */
+  /*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_red,  col_red},
 };
 
 /*  tagging are in binaries     1     2     4     8    16    32    64   128 */
