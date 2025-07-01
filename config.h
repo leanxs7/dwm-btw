c40ea58b262918d65844d999d99d91a24961ec12
s7 <vleandromoises@gmail.com>
2025-07-01 16:33:30 -0300

    chore: tag number nine (9) was removed for convenience.

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 
 /* tagging;                     0    1     2     3     4     5     6     7 */
 /*                              1    2     3     4     5     6     7     8 */
-static const char *tags[] = { " ", " ", "🗁", " ", "📝", " ", " ", " ", " " };
+static const char *tags[] = { " ", " ", "🗁", " ", "📝", " ", " ", " "};
 
 static const Rule rules[] = {
 /* The command to search classes:  xprop | grep "CLASS" */
